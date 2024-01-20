794e08b3980bb8926c12f72965eb05566dcbf1a1
Jossel Redoquerio <josselgredoquerio@iskolarngbayan.pup.edu.ph>
2024-01-21 01:46:57 +0800

    result

--- print_the_sum_of_the_current_and_previous_numbers.py
+++ print_the_sum_of_the_current_and_previous_numbers.py
@@ -15,3 +15,14 @@
 # Current Number 7 Previous Number  6  Sum:  13
 # Current Number 8 Previous Number  7  Sum:  15
 # Current Number 9 Previous Number  8  Sum:  17
+
+print("Printing current and previous number and their sum in a range(10)")
+previous_num = 0
+
+# loop from 1 to 10
+for i in range(1, 11):
+    x_sum = previous_num + i
+    print("Current Number", i, "Previous Number ", previous_num, " Sum: ", x_sum)
+    # modify previous number
+    # set it to the current number
+    previous_num = i
